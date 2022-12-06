871822ec1d421ec0c23c75623204c608067e75c9
r0valdez <rodolfovaldezwork@gmail.com>
2022-12-06 16:41:21 -0600

    Add header

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
 import { ChakraProvider } from '@chakra-ui/react'
+import Layout from '@components/layouts/layout';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </ChakraProvider>
   )
 }
