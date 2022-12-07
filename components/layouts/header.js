6c8b8060ff7bfa429da8807cbb1a3928c19a509f
r0valdez <rodolfovaldezwork@gmail.com>
2022-12-06 19:06:11 -0600

    Header button style

--- components/layouts/header.js
+++ components/layouts/header.js
@@ -6,8 +6,8 @@ const Header = () => {
     <Box display="flex" justifyContent="space-between" alignItems="center" p={4} boxShadow="base">
       <Text display="flex" alignItems="center" gap={1} flexWrap="nowrap" fontSize="2xl" fontWeight="extrabold" color="blue.700"><Icon as={AiFillCloud} boxSize={10} color="blue.300" />Dream Cars</Text>
       <Box display="flex" justifyContent="end" alignItems="center" gap={4} flexWrap="nowrap">
-        <Button colorScheme='gray' variant="outline" fontWeight="bold">Sell a Car</Button>
-        <Button colorScheme='blue'>Sign Up</Button>
+        <Button colorScheme="gray" variant="outline" fontWeight="bold" size="sm">Sell a Car</Button>
+        <Button colorScheme="blue" size="sm">Sign Up</Button>
       </Box>
     </Box>
   );
